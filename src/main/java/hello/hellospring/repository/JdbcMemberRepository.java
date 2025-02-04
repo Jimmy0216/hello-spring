a062cff795ddfcf66fe2d57a23daa3185eac9db2
Jimmy0216 <maximin910216@gmail.com>
2025-02-04 23:55:32 +0900

    스프링 jdbcTemplate - 1

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -39,7 +39,7 @@ public class JdbcMemberRepository implements MemberRepository {
             close(conn, pstmt, rs);
         }
     }
-    @Override
+    @Override //JdbcTemplateMemberRepository에서 findById 똑같은 코드 단 3줄이면 됨
     public Optional<Member> findById(Long id) {
         String sql = "select * from member where id = ?";
         Connection conn = null;
